290b23677a3941ef95ff0d044ec19c43cac95655
GetachewHaftu <getachewhaftu114@gmail.com>
2026-03-12 22:02:01 +0300

    update files

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// Simple welcome message
+window.onload = function() {
+    alert("Welcome to the RAD Lab Project!");
+}
+
+// Button click function
+function showMessage() {
+    document.getElementById("demo").innerHTML =
+    "This project is managed using Git and GitHub.";
+}
